2a3e7525381a7026a4bff47b7b75645bdcfaaa10
Thibaut Rivolet <trivolet@gmail.com>
2020-09-22 06:14:48 +0200

    Base of views

--- product/views.py
+++ product/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.views import View
+from django.views.generic import (
+    DetailView,
+    ListView,
+)
 
-# Create your views here.
+
+class HomeView(View):
+    # view for home
+
+    pass
+
+
+class ProductDetailView(DetailView):
+    # view for product
+
+    pass
+
+
+class ResultListView(ListView):
+    # view for result
+
+    pass
+
+
+class SubstituteListView(ListView):
+    # view for substitutes
+
+    pass
+
--- user/views.py
+++ user/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (
+    CreateView,
+    ListView,
+    UpdateView
+)
 
-# Create your views here.
+
+
+class RegisterCreateView(CreateView):
+    # view for register
+
+    pass
+
+
+class AccountUpdateView(LoginRequiredMixin, UpdateView):
+    # view for myaccount
+
+    pass
+
+
+class SavedListView(LoginRequiredMixin, ListView):
+    # view for myproduct
+
+    pass
+
+
+class SaveCreateView(LoginRequiredMixin, CreateView):
+    # view for save
+
+    pass
